77658790584c4567fa9faefe48fb8c9e13e7e678
Andrea Falco <andrea@falco.sh>
2022-03-17 18:39:48 +0100

    For the moment disable ConcatenationSpacing

--- tests/fixed-doctrine-mod/use-ordering.php
+++ tests/fixed-doctrine-mod/use-ordering.php
@@ -1,15 +1,13 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Foo;
 
+use DateTimeImmutable;
+use DateTimeInterface;
+
 use function sprintf;
 
-use DateTimeImmutable;
-
 use const PHP_EOL;
-
-use DateTimeInterface;
 
 echo sprintf('Current date and time is %s', (new DateTimeImmutable())->format(DateTimeInterface::ATOM)) . PHP_EOL;
--- tests/input/namespaces-spacing.php
+++ tests/input/namespaces-spacing.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 namespace Foo;
 use DateInterval;
-use DateTimeImmutable;
-use DateTimeZone;
 use function strrev;
-use function time;
+use DateTimeImmutable;
 use const DATE_RFC3339;
+use DateTimeZone;
+use function time;
 strrev(
     (new DateTimeImmutable('@' . time(), new DateTimeZone('UTC')))
         ->sub(new DateInterval('P1D'))
